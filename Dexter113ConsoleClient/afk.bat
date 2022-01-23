511450e0362e5f60b5eb25d67f6ad6ab0f563e05
Lauric420 <98264048+Lauric420@users.noreply.github.com>
2022-01-23 15:20:10 +0200

    Add files via upload

---
+++ Dexter113ConsoleClient/afk.bat
@@ -0,0 +1,8 @@
+start /min MinecraftClient.exe c.a.cohen@gmail.com Iloveth3bomb play.chaoticpvp.co
+timeout 5
+
+start /min MinecraftClient.exe abbask908@gmail.com Husainaga1/2 play.chaoticpvp.co
+timeout 5
+
+start /min MinecraftClient.exe h.a.a.y.m@hotmail.com kalbawi1 play.chaoticpvp.co
+timeout 5
